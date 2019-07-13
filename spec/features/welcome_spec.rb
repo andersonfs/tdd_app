2f9dd0d6f46cdf8bf3ec3799e552b5f01bbf002d
anderson.firmino <anderson.firmino@m4u.com.br>
2019-07-13 11:39:31 -0300

    Commit of rails tdd app.

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature "Welcomes", type: :feature do
+  scenario 'Mostra mensagem de Bem-Vindo' do
+    visit(root_path)
+    expect(page).to have_content('Bem-Vindo')
+  end
+
+  scenario 'Veriricar o link cadastro de clientes' do
+    visit(root_path)
+    expect(find('ul li')).to have_link('Cadastro de Clientes')
+  end  
+end
